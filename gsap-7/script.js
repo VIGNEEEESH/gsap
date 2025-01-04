b90c7ea781e884944d1a90f3c200a3580e4e334b
Vignesh <103361940+VIGNEEEESH@users.noreply.github.com>
2025-01-04 14:47:32 +0530

    added gsap 7 animation

---
+++ gsap-7/script.js
@@ -0,0 +1,28 @@
+const marqueAnimation = () => {
+  window.addEventListener("wheel", function (dets) {
+    if (dets.deltaY > 0) {
+      gsap.to("#marque", {
+        transform: "translateX(-200%)",
+        duration: 4,
+        repeat: -1,
+        ease: "none",
+      });
+      gsap.to("#marque img", {
+        rotate: 180,
+      });
+      console.log("rotate1");
+    } else {
+      gsap.to("#marque", {
+        transform: "translateX(0%)",
+        duration: 4,
+        repeat: -1,
+        ease: "none",
+      });
+      console.log("rotate2");
+      gsap.to("#marque img", {
+        rotate: 0,
+      });
+    }
+  });
+};
+marqueAnimation();
